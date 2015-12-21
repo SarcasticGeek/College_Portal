49c7071794820dcef4d5603bccb9e1a3b7b2a91f
Mohamed Elkashif <mohamed_elkashif@windowslive.com>
2015-12-22 01:17:19 +0200

    modifications

--- app/models/index.php
+++ app/models/index.php
@@ -1,3 +1,4 @@
 <?php
 header("Location: ..");
+<a href="pullcourses.php">go to courses</a>
 exit();
--- app/models/pullcourses.php
+++ app/models/pullcourses.php
@@ -6,12 +6,12 @@ if (isset($_POST["student_id"]))
   die('Could not connect: ' . mysql_error());
   mysqli_select_db('college_portal') or die('Could not select database');
   $data  = array();
-  $query = 'SELECT course_id FROM joinedx WHERE student_id='.$id.'';
+  $query = "SELECT course_id FROM joinedx WHERE student_id='$id'";
   $result = mysqli_query($dbconn,$query) or die('Query failed: ' . mysql_error());
   while ($line = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
    
    foreach ($line as $col_value) {
-      	$queryx = 'SELECT code,numberx FROM codename WHERE course_id='.$col_value.'';
+      	$queryx = "SELECT code,numberx FROM codename WHERE course_id='$col_value'";
   		$resultx = mysqli_query($dbconn,$queryx) or die('Query failed: ' . mysql_error());
   		while ($linex = mysqli_fetch_array($resultx, MYSQLI_ASSOC)) {
   			$course_code_number="";
