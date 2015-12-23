3db0534340f6860ce60be86de3a2296f8003d515
mohammed7733 <mohammedessam7733@gmail.com>
2015-12-23 20:31:00 +0200

    course and department functions

---
+++ app/models/course.php
@@ -0,0 +1,47 @@
+<?php
+include "../../dbconnect.php";
+
+class course
+ {
+	public static function get_name($c_id) {
+    global $conn;
+    $sql = "SELECT name FROM course where id = '$c_id'";
+    $result = $conn->query($sql)or die('Query failed: ' . mysql_error());
+    if($result->num_rows >0){
+      $row = mysqli_fetch_assoc($result);
+      return $row['name'];
+    }
+    else{
+      return NULL;
+    }
+  }
+
+  public static function get_name_code($c_id) {
+    global $conn;
+    $sql = "SELECT name_code FROM course where id = '$c_id'";
+    $result = $conn->query($sql)or die('Query failed: ' . mysql_error());
+    if($result->num_rows >0){
+      $row = mysqli_fetch_assoc($result);
+      return $row['name_code'];
+    }
+    else{
+      return NULL;
+    }
+  }
+
+  public static function get_dep_id($c_id) {
+    global $conn;
+    $sql = "SELECT dep_id FROM course where id = '$c_id'";
+    $result = $conn->query($sql)or die('Query failed: ' . mysql_error());
+    if($result->num_rows >0){
+      $row = mysqli_fetch_assoc($result);
+      return $row['dep_id'];
+    }
+    else{
+      return NULL;
+    }
+  }
+
+}
+
+?>
