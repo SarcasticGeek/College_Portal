a9cf6b5d322bfebd1e1e7cbc18ef37baa83a19ab
Hatem93 <hatem.hussin1993@gmail.com>
2015-12-24 02:02:40 +0800

    get tas of function

--- app/models/student.php
+++ app/models/student.php
@@ -19,7 +19,7 @@ Class Student
 	public static function get_Year($id)
 	{
 		global $conn;
-    	$sql = "SELECT studyingyear FROM student WHERE p_id ='.$id'";
+    	$sql = "SELECT studyingyear FROM student WHERE p_id ='$id'";
     	$result = $conn->query($sql)or die('Query failed: ' . mysql_error());
 		if ($result->num_rows > 0)
 		{
@@ -52,7 +52,7 @@ Class Student
 	public static function get_Courses($id)
 	{
 		global $conn;
-		$sql = "SELECT c_id FROM joined WHERE student_id ='.$id'";
+		$sql = "SELECT c_id FROM joined WHERE student_id ='$id'";
 		if ($result->num_rows > 0)
 		{
 			$courses = array();
@@ -60,6 +60,24 @@ Class Student
 				array_push($courses,$row['c_id']);
 			}
     		return $courses;
+		}
+    	else 
+    	{
+    		return 0;
+		}
+
+	}
+	public static function get_Tas_OfCourse($id)
+	{
+		global $conn;
+		$sql = "SELECT st_id FROM teach WHERE c_id ='$id'";
+		if ($result->num_rows > 0)
+		{
+			$Tas = array();
+			while($row = $result->fetch_assoc()) {
+				array_push($Tas,$row['st_id']);
+			}
+    		return $Tas;
 		}
     	else 
     	{
