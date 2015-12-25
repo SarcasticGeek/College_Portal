afa8c1f8125f799862f96d4839e68520e618652e
Mohamed Elkashif <mohamed_elkashif@windowslive.com>
2015-12-25 16:44:52 +0200

    Message front end

--- app/views/stuff/messages.php
+++ app/views/stuff/messages.php
@@ -10,7 +10,9 @@
     <style >
     .new {
         text-align: left;
-       
+       position: relative;
+       width: 1000px;
+       left: 20px;
         border-radius:5px;
         margin:-13px;
         background-color: orange;
@@ -24,7 +26,7 @@
      	position: relative;
      	
         text-align: left;
-        background-color: lightgray;
+        
         border-radius:5px;
          margin:-13px;
 
@@ -44,6 +46,10 @@
     	position: relative;
     	left: 25px;
     }
+    .openModal
+    {
+
+    }
     </style>
   </head>
   <body>
@@ -51,7 +57,36 @@
  <div class = "newmessages">
 <h1>New messages</h1>
  <div class="new">
- 	 <p id="empty">No new messages</p>
+
+ 	
+     <p > How are you? by Mohamed Elkashif <a type="link" class="primary" data-toggle="modal" data-target="#ToStuff">Answer</a>
+        <div class="modal fade" id="ToStuff" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">×</span>
+                    </button>
+                    <h4 class="modal-title" id="myModalLabel" style="color:black;">Answer to Student: Kashif</h4>
+                  </div>
+                  <div class="modal-body">
+                    <form class="form-horizontal" role="form" action="send.php" >
+                      <div class="form-group">
+          
+                        <div class="col-sm-2">
+                          <label for="inputPassword3" class="control-label">Answer</label>
+                        </div>
+                        <div class="col-sm-8">
+                          <textarea class="form-control" name="answer" id="inputPassword3" placeholder="Put your passwrod here"></textarea>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                          <button type="submit" class="btn btn-default">Answer</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
  </div>
  </div>
 </body></html>
