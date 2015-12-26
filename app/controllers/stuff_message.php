97c37b07dc6548a1c71a75bd3660cb5cc8b9d7f7
Mohamed Essam Fathalla <m_essamfathalla@yahoo.com>
2015-12-26 13:49:15 +0200

    fix message in stuff

--- app/controllers/stuff_message.php
+++ app/controllers/stuff_message.php
@@ -7,7 +7,7 @@ include $_SERVER["DOCUMENT_ROOT"] . "/College_Portal/app/models/Stuff.php";
 include $_SERVER["DOCUMENT_ROOT"] . "/College_Portal/app/models/course.php";
  if(isset($_POST["question"]) && isset($_POST["s_id"]) && isset($_POST["answer"]) && isset($_POST['c_id']) )
  {
-    $data = Stuff::answer($_POST["c_id"],$_POST["s_id"],$_POST["question"],isset($_POST["answer"]));
+    $data = Stuff::answer($_POST["c_id"],$_POST["s_id"],$_POST["question"],$_POST["answer"]);
    if($data) echo"Message sent";
    else echo"Message hasn't been sent";
  }
